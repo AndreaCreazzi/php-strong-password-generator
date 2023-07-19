1b7f039e66f6a8b768e1af68e960608f251db2a8
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-19 14:43:23 +0200

    add Milestone 1

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
+function generateRandomPassword($length)
+{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+    $password = '';
+    $charCount = strlen($chars) - 1;
 
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, $charCount)];
+    }
+
+    return $password;
+}
+
+$error_message = '';
+$generated_password = '';
+
+
+if (isset($_GET['password_length'])) {
+    $password_length = $_GET['password_length'];
+
+    if (empty($password_length) || !is_numeric($password_length)) {
+        $error_message = "Inserisci una lunghezza valida per la password.";
+    } else {
+        $password_length = (int)$password_length;
+        $generated_password = generateRandomPassword($password_length);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,12 +45,16 @@
             <h2>Genera una password sicura</h2>
             <div class="card mt-4">
                 <div class="card-header">
-                    <div class="alert alert-info"></div>
+                    <?php if (!empty($error_message)) : ?>
+                        <div class="alert alert-danger"><?php echo $error_message; ?></div>
+                    <?php elseif (isset($generated_password)) : ?>
+                        <div class="alert alert-info">Password generata: <?php echo $generated_password; ?></div>
+                    <?php endif; ?>
                 </div>
                 <div class="card-body">
                     <form action="" method="GET">
                         <label for="password">Lungezza password</label>
-                        <input class="mx-3" id="password" type="number" placeholder="Inserisci un numero">
+                        <input class="mx-3" id="password" name="password_length" type="number" placeholder="Inserisci un numero">
                         <button class="btn btn-success" type="submit">Crea</button>
                     </form>
                 </div>
